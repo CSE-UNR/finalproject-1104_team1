be2b7c7424b7fc8f4c3d86416c13b159c680da39
Brysen Ross <brysenr@nevada.unr.edu>
2024-05-02 20:20:22 +0000

    image

--- images.c
+++ images.c
@@ -1,6 +1,16 @@
 //Author: Sergio Jackson, Brysen Ross
 
 #include <stdio.h>
+#define MAXCOL 500
+#define MAXROW 500
+void loadimage(int imagearray[][MAXCOL], int* rowptr, int* colptr);
+void displayimage();
+void editimage();
+void cropimage();
+void brightenimage();
+void dimimage();
+void rotateimage();
+void saveimage();
 
 int main(){
 
